cdcdd08af047e6e9c1633be5f88754d8375d2427
Everardo Alvaro Agustin Cruz <ever234.ea@gmsil.com>
2023-07-04 15:12:15 -0500

    Se agrego el archivo yarn.lock server styleci

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,10 +2,12 @@
 echo "corriendo Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-
+composer show 
 echo " corriendo Caching config..."
 php artisan config:cache
 
+echo "Limpiar las rutas"
+php artisan cache:clear
 echo "ever Caching routes..."
 php artisan route:cache
 
