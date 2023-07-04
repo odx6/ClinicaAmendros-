28a12fcbd8fe7d1b7cb1a121a37fef567890efd4
Everardo Alvaro Agustin Cruz <ever234.ea@gmsil.com>
2023-07-04 13:38:26 -0500

    Se agrego el archivo yarn.lock server styleci

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Routing\UrlGenerator;
+//use Illuminate\Routing\UrlGenerator;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV') == 'production') {
+       /* if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
-        }
+        }*/
     }
 }
