5d353c5df245de2023211d35297c4d978b397b1f
Everardo Alvaro Agustin Cruz <ever234.ea@gmsil.com>
2023-07-04 02:06:34 -0500

    Se agrego archivo de ejecucion

---
+++ Deploy.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+echo  " Compositor en ejecución "
+compositor global requiere hirak/prestissimo
+instalación del compositor --no-dev --working-dir=/var/www/html
+
+echo  " Configuración de almacenamiento en caché... "
+configuración artesanal de php: caché
+
+echo  " Almacenamiento en caché de rutas... "
+ruta artesanal de php: caché
+
+echo  " Ejecutando migraciones... "
+php artesanal migrar --fuerza
